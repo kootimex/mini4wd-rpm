75f76cf4f305a20845c5b91b238ede2fdbdc6a1e
kootimex <kootimex@gmail.com>
2026-08-24 00:24:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mini4wd-rpm-v6';
+const CACHE = 'mini4wd-rpm-v7';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
